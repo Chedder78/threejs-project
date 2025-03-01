370690f201ac7873fc5e3cbd676e6bcaea5da1e7
chad morrison <134284764+Chedder78@users.noreply.github.com>
2025-02-28 18:04:12 -0800

    Update CopyShader.module.js

--- CopyShader.module.js
+++ CopyShader.module.js
@@ -1,16 +1,12 @@
 /**
  * Full-screen textured quad shader
  */
-
 const CopyShader = {
-
 	name: 'CopyShader',
 
 	uniforms: {
-
 		'tDiffuse': { value: null },
 		'opacity': { value: 1.0 }
-
 	},
 
 	vertexShader: /* glsl */`
@@ -18,28 +14,27 @@ const CopyShader = {
 		varying vec2 vUv;
 
 		void main() {
-
 			vUv = uv;
 			gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-
-		}`,
+		}
+	`,
 
 	fragmentShader: /* glsl */`
 
-		uniform float opacity;
+		precision mediump float;
 
+		uniform float opacity;
 		uniform sampler2D tDiffuse;
 
 		varying vec2 vUv;
 
 		void main() {
-
 			vec4 texel = texture2D( tDiffuse, vUv );
-			gl_FragColor = opacity * texel;
 
-
-		}`
-
+			// Preserve the original alpha while applying opacity to RGB channels
+			gl_FragColor = vec4(texel.rgb * opacity, texel.a);
+		}
+	`
 };
 
 export { CopyShader };
