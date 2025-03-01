643fea04a6bd7e03791db112f3f58df5fbe36fd1
chad morrison <134284764+Chedder78@users.noreply.github.com>
2025-02-28 18:16:37 -0800

    Update ShaderPass.module.js

--- ShaderPass.module.js
+++ ShaderPass.module.js
@@ -5,73 +5,53 @@ import {
 import { Pass, FullScreenQuad } from './Pass.module.js';
 
 class ShaderPass extends Pass {
-
-	constructor( shader, textureID ) {
-
+	constructor(shader, textureID) {
 		super();
 
-		this.textureID = ( textureID !== undefined ) ? textureID : 'tDiffuse';
+		this.textureID = textureID !== undefined ? textureID : 'tDiffuse';
 
-		if ( shader instanceof ShaderMaterial ) {
-
+		if (shader instanceof ShaderMaterial) {
 			this.uniforms = shader.uniforms;
-
 			this.material = shader;
+		} else if (shader) {
+			this.uniforms = UniformsUtils.clone(shader.uniforms);
 
-		} else if ( shader ) {
-
-			this.uniforms = UniformsUtils.clone( shader.uniforms );
-
-			this.material = new ShaderMaterial( {
-
-				name: ( shader.name !== undefined ) ? shader.name : 'unspecified',
-				defines: Object.assign( {}, shader.defines ),
+			this.material = new ShaderMaterial({
+				name: shader.name || '',
+				defines: Object.assign({}, shader.defines),
 				uniforms: this.uniforms,
 				vertexShader: shader.vertexShader,
 				fragmentShader: shader.fragmentShader
-
-			} );
-
+			});
 		}
 
-		this.fsQuad = new FullScreenQuad( this.material );
-
+		this.fsQuad = new FullScreenQuad(this.material);
 	}
 
-	render( renderer, writeBuffer, readBuffer /*, deltaTime, maskActive */ ) {
-
-		if ( this.uniforms[ this.textureID ] ) {
-
-			this.uniforms[ this.textureID ].value = readBuffer.texture;
-
+	render(renderer, writeBuffer, readBuffer /*, deltaTime, maskActive */) {
+		// Ensure uniform exists before assignment
+		if (this.uniforms && this.uniforms[this.textureID]) {
+			this.uniforms[this.textureID].value = readBuffer.texture;
 		}
 
 		this.fsQuad.material = this.material;
 
-		if ( this.renderToScreen ) {
-
-			renderer.setRenderTarget( null );
-			this.fsQuad.render( renderer );
-
+		if (this.renderToScreen) {
+			renderer.setRenderTarget(null);
 		} else {
+			renderer.setRenderTarget(writeBuffer);
 
-			renderer.setRenderTarget( writeBuffer );
-			// TODO: Avoid using autoClear properties, see https://github.com/mrdoob/three.js/pull/15571#issuecomment-465669600
-			if ( this.clear ) renderer.clear( renderer.autoClearColor, renderer.autoClearDepth, renderer.autoClearStencil );
-			this.fsQuad.render( renderer );
-
+			// Properly clear the buffer before rendering
+			if (this.clear) renderer.clear(true, true, true);
 		}
 
+		this.fsQuad.render(renderer);
 	}
 
 	dispose() {
-
-		this.material.dispose();
-
-		this.fsQuad.dispose();
-
+		if (this.material) this.material.dispose();
+		if (this.fsQuad) this.fsQuad.dispose();
 	}
-
 }
 
 export { ShaderPass };
